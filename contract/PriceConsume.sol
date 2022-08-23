5a4f749b00435254ca7bff577d912643c6132854
VIVEK-SUTHAR <sutharvivek18@gmail.com>
2022-08-23 19:52:59 +0530

    Price feed added

--- contract/PriceConsume.sol
+++ contract/PriceConsume.sol
@@ -1,7 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
-
-// Import this file to use console.log
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract PriceConsume {
